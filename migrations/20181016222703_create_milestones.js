271b16d51e0e43aba69d3f76aba0d02fdced6f0d
Jenny Poon <jenny.poon@live.ca>
2018-10-16 23:04:08 +0000

    added primary key to table

--- migrations/20181016222703_create_milestones.js
+++ migrations/20181016222703_create_milestones.js
@@ -1,6 +1,7 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('milestone', (table) => {
+    table.increments('id');
     table.string('description');
     table.date('date_achieved');
   })
